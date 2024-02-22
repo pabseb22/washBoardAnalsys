926b0f62dbb40ecde3cb719ae64ad0d0d0d35bda
pabseb22 <pablosebas2002@hotmail.com>
2024-02-22 08:06:27 -0500

    - Added Scipy optimization to minimize - Pending: Retain original surface

---
+++ particleSwarmOptimization/cellbedform_PSO.py
@@ -0,0 +1,83 @@
+
+import numpy as np
+
+class CellBedform():
+
+    def __init__(self, grid=(100, 50), D=0.8, Q=0.6, L0=7.3, b=2.0, y_cut=10):
+        # Copy input parameters
+        self._xgrid = grid[0]
+        self._ygrid = grid[1]
+        self.D = D
+        self.Q = Q
+        self.L0 = L0
+        self.b = b
+
+        # Make initial topography
+        self.h = np.random.rand(self._xgrid, self._ygrid)
+        self.initial_h = self.h
+        self.L = np.empty(self.h.shape)
+        self.dest = np.empty(self.h.shape)
+
+        # Make arrays for indeces showing grid of interest and neighbor grids
+        self.y, self.x = np.meshgrid(
+            np.arange(self._ygrid), np.arange(self._xgrid))
+        self.xminus = self.x - 1
+        self.xplus = self.x + 1
+        self.yminus = self.y - 1
+        self.yplus = self.y + 1
+
+        # Periodic boundary condition
+        self.xminus[0, :] = self._xgrid - 1
+        self.xplus[-1, :] = 0
+        self.yminus[:, 0] = self._ygrid - 1
+        self.yplus[:, -1] = 0
+
+        # Variables for visualization
+        self.y_cuts = []
+        self.y_cut = y_cut
+
+    def run(self, steps=100):
+        for i in range(steps):
+            self.run_one_step()
+            self.y_cuts.append([np.arange(self._xgrid), self.h[:, self.y_cut]])
+
+        #Compare results
+        return self.y_cuts, self.initial_h
+
+    def run_one_step(self):
+        """Calculate one step of the model
+        """
+        x = self.x
+        y = self.y
+        xplus = self.xplus
+        yplus = self.yplus
+        xminus = self.xminus
+        yminus = self.yminus
+        D = self.D
+        Q = self.Q
+        L0 = self.L0
+        b = self.b
+        L = self.L
+        dest = self.dest
+        # Rolling and sliding
+        # Amounts of sediment transported to/from adjacent grids are 1/6 D h,
+        # and those to/from diagonal grids are 1/12 D h
+        self.h = self.h + D * (-self.h + 1. / 6. *
+                               (self.h[xplus, y] + self.h[xminus, y] + self.
+                                h[x, yplus] + self.h[x, yminus]) + 1. / 12. *
+                               (self.h[xplus, yplus] + self.h[xplus, yminus] +
+                                self.h[xminus, yplus] + self.h[xminus, yplus]))
+
+        # Saltation
+        # Length of saltation is determined as:
+        # L = L0 + b h
+        # Thus, particles at higher elevation travel longer
+        L = L0 + b * self.h  # Length of saltation
+        L[np.where(L < 0)] = 0  # Avoid backward saltation
+        np.round(L + x, out=dest)  # Grid number must be integer
+        np.mod(dest, self._xgrid, out=dest)  # periodic boundary condition
+        self.h = self.h - Q  # Entrainment
+        for j in range(self.h.shape[0]):  # Settling
+            self.h[dest[j, :].astype(np.int32),
+                   y[j, :]] = self.h[dest[j, :].astype(np.int32), y[j, :]] + Q
+
--- particleSwarmOptimization/main_PSO.py
+++ particleSwarmOptimization/main_PSO.py
@@ -1,21 +1,58 @@
 import numpy as np
 import pyswarms as ps
+from cellbedform_PSO import CellBedform
+from scipy.optimize import minimize
+
+# Problemas: 
+# Contra que comparo
+# Como el optimizer puede encontrar algo si la funcion me devuelve por cada step un corte en x, verifica cada step?
+# Que corte elegimos, analiza todos?
+# En caso de que pueda optimizar algo, como podemos replicar sus resultados si se genera una superficie aleatoria. Debo dejar seteada una misma superficie?
+# Que parametros deberia optimizar?
+# Cuantos steps seran suficientes?
+
+D = 0.8
+Q = 0.6
+
+dx = 150
+dy = 40
+y_cut = 20
+steps = 101
+
+def wave1(x, a, b, c):
+    return a * np.sin(b * x + c)
 
 # Objective function to minimize (quadratic function)
-def objective_function(x):
-    return np.sum(x**2)
+def objective_function(params):
+    L0_, b_ = params
+    cb = CellBedform(grid=(dx, dy), D=D, Q=Q, L0=L0_, b=b_, y_cut=y_cut)
+    y_cuts, initial_h = cb.run(steps)
+    diff = y_cuts[100] - wave1(2,1,3,1)
+    return np.sum(diff**2)
+
+# Initial guess for the parameters
+params_initial = [7.3, 2]
+
+# Call the optimizer
+result = minimize(objective_function, params_initial, method='Nelder-Mead')
+
+# The optimal parameters are stored in result.x
+print(result)
+print(result.x)
 
 # Initialize the swarm
-num_particles = 10
-dimensions = 3
-options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+# num_particles = 10
+# dimensions = 3
+# options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
 
-# Create a Particle Swarm Optimizer
-optimizer = ps.single.GlobalBestPSO(n_particles=num_particles, dimensions=dimensions, options=options)
+# # Create a Particle Swarm Optimizer
+# optimizer = ps.single.GlobalBestPSO(n_particles=num_particles, dimensions=dimensions, options=options)
 
-# Run the optimization
-best_position, _ = optimizer.optimize(objective_function, iters=100)
+# # Run the optimization
+# best_position, _ = optimizer.optimize(objective_function, iters=100)
 
-# Display the result
-print("Best Position:", best_position)
-print("Objective Value at Best Position:", objective_function(best_position))
+# # Display the result
+# print("Best Position:", best_position)
+# print("Objective Value at Best Position:", objective_function(best_position))
+
+
